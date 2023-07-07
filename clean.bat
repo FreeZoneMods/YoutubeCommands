36805b25a20dd2d9cf37c47eea7a40c9a9a4c452
gunslingermod <stfreeplayzone@yandex.ru>
2023-07-07 13:44:21 +1000

    Батник очистки от мусора

---
+++ clean.bat
@@ -0,0 +1,15 @@
+rmdir /S /Q bin
+rmdir /S /Q obj
+rmdir /S /Q packages
+rmdir /S /Q .vs
+del ChatInteractiveCommands.csproj.user
+del scores.db
+del StalkerInteractiveCommandsProcessor.ini
+
+
+rmdir /S /Q StalkerInteractiveCommandsProcessor\backup
+rmdir /S /Q StalkerInteractiveCommandsProcessor\lib
+del StalkerInteractiveCommandsProcessor\in.ini
+del StalkerInteractiveCommandsProcessor\out.ini
+del StalkerInteractiveCommandsProcessor\*.exe
+del StalkerInteractiveCommandsProcessor\*.lps
